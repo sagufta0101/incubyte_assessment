893fea9adc6c686b9916d8e8054875e7a76c7bf5
sagufta0101 <spsysorexedge@gmail.com>
2025-02-12 11:43:05 +0530

    feat: add - Passing n (unknown) number of nums separated by newline delimeter should return their sum [2/3] - Test Results:
    00:00 +0: Empty string returns 0
    
    00:00 +1: Single number returns the number
    
    00:00 +2: Two numbers returns their sum
    
    00:00 +3: Multiple numbers returns their sum
    
    00:00 +4: Numbers separated by newline delimeter
    
    00:00 +5: All tests passed!
    
    Exited.

--- calculate_string.dart
+++ calculate_string.dart
@@ -1,7 +1,7 @@
 // Function to add numbers from given string.
 int add(String numbers) {
   if (numbers.isEmpty) return 0;
-
+  numbers = numbers.replaceAll("\n", ",");
   return numbers.split(",").map(int.parse).fold(
         0,
         (previousValue, nextValue) => previousValue + nextValue,
