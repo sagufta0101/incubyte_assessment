1436da563dfd2bb8e1f5f9f25e32718a07e641dc
sagufta0101 <spsysorexedge@gmail.com>
2025-02-12 11:37:39 +0530

    feat: add - Passing n (unknown) number of nums should return their sum [2/3] - Test Results:
    00:00 +0: Empty string returns 0
    
    00:00 +1: Single number returns the number
    
    00:00 +2: Two numbers returns their sum
    
    00:00 +3: Multiple numbers returns their sum
    
    00:00 +4: All tests passed!
    
    Exited.

--- calculate_string.dart
+++ calculate_string.dart
@@ -3,13 +3,11 @@ int add(String numbers) {
   int returningVal = 0;
   if (numbers.isEmpty) {
     return returningVal;
-  } else {
-    if (numbers.contains(",")) {
-      List<String> parts = numbers.split(',');
-      returningVal = int.parse(parts[0]) + int.parse(parts[1]);
-    } else {
-      returningVal = int.parse(numbers);
-    }
-    return returningVal;
   }
+
+  List<String> parts = numbers.split(",");
+  for (final part in parts) {
+    returningVal += int.parse(part);
+  }
+  return returningVal;
 }
