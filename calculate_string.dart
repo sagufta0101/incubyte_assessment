52d2779879130c8b6eb83c5ea881f83e05e3b2a4
sagufta0101 <spsysorexedge@gmail.com>
2025-02-12 11:46:16 +0530

    refactor: Passing n (unknown) number of nums separated by newline delimeter should return their sum [3/3] - Use regex to split with multiple delimters
    - Test Results:
    
    00:00 +0: Empty string returns 0
    
    00:00 +1: Single number returns the number
    
    00:00 +2: Two numbers returns their sum
    
    00:00 +3: Multiple numbers returns their sum
    
    00:00 +4: Numbers separated by newline delimeter
    
    00:00 +5: All tests passed!
    
    Exited.

--- calculate_string.dart
+++ calculate_string.dart
@@ -1,9 +1,16 @@
 // Function to add numbers from given string.
 int add(String numbers) {
   if (numbers.isEmpty) return 0;
-  numbers = numbers.replaceAll("\n", ",");
-  return numbers.split(",").map(int.parse).fold(
+
+  RegExp delimeters = RegExp(r'[\n,]');
+  return numbers.split(delimeters).map(int.parse).fold(
         0,
         (previousValue, nextValue) => previousValue + nextValue,
       );
+}
+
+void main() {
+  String input = '1\n2,3';
+  int result = add(input);
+  print('The sum of the numbers is: $result');
 }
