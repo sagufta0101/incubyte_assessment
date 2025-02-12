89901310fb8b08c90874186569a0d51708158e43
sagufta0101 <spsysorexedge@gmail.com>
2025-02-12 10:52:13 +0530

    refactor: add - Passing one number should return the number itself [3/3] - Test Results:
    00:00 +0: Empty string returns 0
    
    00:00 +1: Single number returns the number
    
    00:00 +2: All tests passed!
    
    Exited.

--- calculate_string.dart
+++ calculate_string.dart
@@ -1,6 +1,6 @@
 // Function to add numbers from given string.
 int add(String numbers) {
-  if (numbers == "") {
+  if (numbers.isEmpty) {
     return 0;
   } else {
     int num = int.parse(numbers);
