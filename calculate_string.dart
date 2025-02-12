c30a302e9b3f32a00a7b1eaac481fda8bb288799
sagufta0101 <spsysorexedge@gmail.com>
2025-02-12 11:01:47 +0530

    feat: add - Passing 2 numbers separater by , should return their sum [2/3] - Test Results:
    00:00 +0: Empty string returns 0
    
    00:00 +1: Single number returns the number
    
    00:00 +2: Two numbers returns their sum
    
    00:00 +3: All tests passed!
    
    Exited.

--- calculate_string.dart
+++ calculate_string.dart
@@ -3,6 +3,11 @@ int add(String numbers) {
   if (numbers.isEmpty) {
     return 0;
   } else {
+    if (numbers.contains(",")) {
+      List<String> parts = numbers.split(',');
+      int sum = int.parse(parts[0]) + int.parse(parts[1]);
+      return sum;
+    }
     int num = int.parse(numbers);
     return num;
   }
