9e68926b7bf7a748ad42cede4188b44ef5cc6b94
sagufta0101 <spsysorexedge@gmail.com>
2025-02-12 11:40:09 +0530

    refactor: add - Passing n (unknown) number of nums should return their sum [3/3] - Cleanup returns and use dart built in methods rather than largely unoptimised for loop
    - Test Results:
    
    00:00 +0: Empty string returns 0
    
    00:00 +1: Single number returns the number
    
    00:00 +2: Two numbers returns their sum
    
    00:00 +3: Multiple numbers returns their sum
    
    00:00 +4: All tests passed!
    
    Exited.

--- calculate_string.dart
+++ calculate_string.dart
@@ -1,13 +1,9 @@
 // Function to add numbers from given string.
 int add(String numbers) {
-  int returningVal = 0;
-  if (numbers.isEmpty) {
-    return returningVal;
-  }
+  if (numbers.isEmpty) return 0;
 
-  List<String> parts = numbers.split(",");
-  for (final part in parts) {
-    returningVal += int.parse(part);
-  }
-  return returningVal;
+  return numbers.split(",").map(int.parse).fold(
+        0,
+        (previousValue, nextValue) => previousValue + nextValue,
+      );
 }
