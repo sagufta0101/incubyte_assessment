8c565f666ba786a8d9b2c4a4217da1ff8367e0e8
sagufta0101 <spsysorexedge@gmail.com>
2025-02-12 10:49:29 +0530

    test: add - Passing one number should return the number itself [1/3] - Test Results:
    00:00 +0: Empty string returns 0
    
    00:00 +1: Single number returns the number
    
    00:00 +1 -1: Single number returns the number [E]
    
      Expected: <1>
        Actual: <0>
    
    00:00 +1 -1: Some tests failed.

--- calculate_string_test.dart
+++ calculate_string_test.dart
@@ -5,4 +5,7 @@ void main() {
   test('Empty string returns 0', () {
     expect(add(''), equals(0));
   });
+  test('Single number returns the number', () {
+    expect(add('1'), equals(1));
+  });
 }
